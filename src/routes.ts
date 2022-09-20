5c028ff8ebb9ad3255e4e5d2e6a0615030c9d1ca
Samuel Bruno <samdg919@gmail.com>
2022-09-20 10:59:57 -0300

    Implemented 404 route

--- src/routes.ts
+++ src/routes.ts
@@ -9,5 +9,12 @@ routes.post('/book', BooksController.addBook)
 routes.put('/book', BooksController.editBook)
 routes.delete('/book', BooksController.deleteBook)
 
+routes.use((req, res, next) => {
+  res.status(404).json({
+    success:false,
+    error:'Route not exists'
+  })
+})
+
 
 export default routes
