a2c5dbc98a21af48714cbf9b0adbc49fbecd25a1
Samuel Bruno <samdg919@gmail.com>
2022-09-20 10:28:09 -0300

    Implemented 'get book by id' route

--- src/controllers/BooksController.ts
+++ src/controllers/BooksController.ts
@@ -10,3 +10,28 @@ export const getAllBooks = async (req:Request, res:Response) => {
     books
   })
 }
+
+export const getBookById = async (req: Request, res: Response) => {
+  const id = req.params.bookId
+
+  if (id) {
+    const book = await Book.findByPk(id)
+
+    if (book !== null) {
+      res.status(200).json({
+        success: true,
+        book
+      })
+    } else {
+      res.status(400).json({
+        success: false,
+        error: 'Livro não encontrado'
+      })
+    }
+  } else {
+    res.status(400).json({
+      success: false,
+      error: 'Erro ao buscar livro. Tente novamente mais tarde'
+    })
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import * as BooksController from './controllers/BooksController'
 const routes = Router()
 
 routes.get('/books', BooksController.getAllBooks)
+routes.get('/book/:bookId', BooksController.getBookById)
 
 
 export default routes
